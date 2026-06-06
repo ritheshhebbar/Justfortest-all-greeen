3b5bf7bfeaf37d8742849c0620889ba74967bdc5
ritheshhebbar <ksshashikanthahebbar@gmail.com>
2026-06-06 12:27:35 +0000

    new

--- main.sh
+++ main.sh
@@ -1,28 +1,34 @@
 git init
 
+git config user.name "ritheshhebbar"
+git config user.email "ksshashikanthahebbar@gmail.com"
+
 touch junk
 
-declare -i x
 echo "Enter Beginning date [yyyymmdd]"
 read x
 
-declare -i y
 echo "Enter End date [yyyymmdd]"
 read y
 
 DATE=$x
 
 while [ $DATE -le $y ]
-
 do
-	echo 'a' >> junk
+    for i in {1..10}
+    do
+        echo "$i" >> junk
 
-	git add .
+        git add junk
 
-	msg='commit'${DATE}
+        msg="commit${DATE}-${i}"
 
-	git commit -m $msg --date="$(date -R -d ${DATE})"
-   	
-   	DATE=$(date +%Y%m%d -d "$DATE + 1 day")
+        GIT_AUTHOR_NAME="ritheshhebbar" \
+        GIT_AUTHOR_EMAIL="ksshashikanthahebbar@gmail.com" \
+        GIT_COMMITTER_NAME="ritheshhebbar" \
+        GIT_COMMITTER_EMAIL="ksshashikanthahebbar@gmail.com" \
+        git commit -m "$msg" --date="$(date -R -d ${DATE})"
+    done
 
+    DATE=$(date +%Y%m%d -d "$DATE + 1 day")
 done
